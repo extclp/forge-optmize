271e9bd15fc1e8ad5f2a5bed27c570c871793597
extclp <2842417407@qq.com>
2023-09-25 07:01:20 +0800

    Remove dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,6 @@ plugins {
     java
 }
 
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.ow2.asm:asm:9.5")
-    implementation("org.ow2.asm:asm-tree:9.5")
-}
-
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
